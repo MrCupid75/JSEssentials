959e5998f46f09d18aa668ee09b96295c7a75518
Beast <mjnuvor@gmail.com>
2025-02-02 18:02:56 +0000

    authentication  Status added

--- control_flow/control_flow.js
+++ control_flow/control_flow.js
@@ -46,3 +46,9 @@ switch (userType) {
 }
 
 console.log("User category:", userCategory)
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
