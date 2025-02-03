4fca8cbf03ee5ceeaf0e15acbeb6b6b0a67380ec
Beast <mjnuvor@gmail.com>
2025-02-03 08:49:02 +0000

    counter follower feature

---
+++ jsfunctions/follower_counter.js
@@ -0,0 +1,17 @@
+const countDisplay = document.querySelector("#countDisplay")
+
+let count = 0;
+
+const increaseCount = () => {
+    count++;
+    countDisplay.innerHTML = count;
+    checkCountValue()
+}
+
+const checkCountValue = () => {
+    if (count === 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!")
+    } else if (count === 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!")
+    }
+}
